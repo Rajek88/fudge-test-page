d1ef4f3079666ab22f66b6fd4d07bf5de3815f06
Rajendra Kulkarni <rajerkulkarni01@gmail.com>
2024-05-10 19:49:53 +0530

    able to render and create all invitations on admin page

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -3,7 +3,8 @@ import { Input } from "components/Input";
 import { Page } from "components/Page";
 import { Select } from "components/Select";
 import TableSetup from "components/tableSetup/TableSetup";
-import React, { useState } from "react";
+import { USE_BASE_URL } from "config/URL";
+import React, { useEffect, useState } from "react";
 import { validateEmail } from "utils/validators/Validators";
 
 type Props = {};
@@ -12,6 +13,7 @@ const Admin = (props: Props) => {
   const [email, setEmail] = useState<string>("");
   const [team, setTeam] = useState<string>("A");
   const [rows, setRows] = useState<object[]>([]);
+  const [update, setUpdate] = useState<number>(0);
   const teams = ["A", "B", "C", "D"];
   const [err, setErr] = useState<boolean>(false);
 
@@ -31,17 +33,65 @@ const Admin = (props: Props) => {
     setEmail(mailId);
   };
 
-  const inviteUser = () => {
-    setRows((p) => [
-      ...p,
-      {
-        "Sr. no": p.length + 1,
-        email: email,
-        status: "invited",
-        time: Date.now(),
-      },
-    ]);
+  const inviteUser = async () => {
+    if (!email || !team) {
+      setErr(true);
+    }
+    try {
+      const res = await fetch(`${USE_BASE_URL}/admin/inviteUser`, {
+        method: "POST",
+        body: JSON.stringify({
+          email,
+          team,
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error();
+      }
+      // update counter, to trigger page refresh
+      setUpdate((v) => v + 1);
+      // reset email field
+      setEmail("");
+    } catch (error) {}
   };
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${USE_BASE_URL}/admin/getAllInvitations`, {
+        method: "GET",
+      });
+
+      if (!res.ok) {
+        throw new Error();
+      }
+
+      // we are here means req is ok
+      const data = await res.json();
+      // now get the list of teamMembers
+      const invitations = data?.invitations;
+      // now loop over invitations and generate renderable data
+      let rowsToRender = [];
+      let count = 1;
+      for (let i of invitations) {
+        let r = {
+          "Sr. no": count,
+          email: i.email,
+          status: i.status,
+          team: i.team,
+          time: i.updated_at,
+        };
+        count++;
+        rowsToRender.push(r);
+      }
+      // now set the rows
+      setRows(rowsToRender);
+    }
+
+    fetchData();
+
+    return () => {};
+  }, [update]);
 
   return (
     <Page
@@ -68,7 +118,7 @@ const Admin = (props: Props) => {
       <div>
         <h2 className="py-2 font-semibold">Invitations sent.</h2>
         <TableSetup
-          columns={["Sr. no", "email", "status", "time"]}
+          columns={["Sr. no", "email", "team", "status", "time"]}
           rows={rows}
         />
       </div>
